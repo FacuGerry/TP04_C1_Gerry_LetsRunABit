3ac64b5547d3afa8b80c8934a3c3c745bb3d8001
Facundo Gerry <facuto365pro@gmail.com>
2025-09-13 14:52:17 -0300

    Main Menu optimization and new sprites

--- Assets/Scripts/MainMenu/ButtonsMainMenu.cs
+++ Assets/Scripts/MainMenu/ButtonsMainMenu.cs
@@ -1,18 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ButtonsMainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Button playBtn;
+    [SerializeField] private Button creditsBtn;
+    [SerializeField] private Button exitBtn;
+
     void Start()
     {
-        
+        playBtn.onClick.AddListener(OnPlayClicked);
+        creditsBtn.onClick.AddListener(OnCreditsClicked);
+        exitBtn.onClick.AddListener(OnExitClicked);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        playBtn.onClick.RemoveAllListeners();
+        creditsBtn.onClick.RemoveAllListeners();
+        exitBtn.onClick.RemoveAllListeners();
     }
+
+    public void OnPlayClicked()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    private void OnExitClicked()
+    {
+        Application.Quit();
+    }
+
+    private void OnCreditsClicked()
+    {
+
+    }
+
 }
